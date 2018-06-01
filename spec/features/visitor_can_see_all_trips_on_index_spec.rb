1b47f4c34b3aca56f6bbe8513cfaf6693d16c24b
Justine <jjtroyke@gmail.com>
2018-06-01 10:03:57 -0600

    2nd story passing

--- spec/features/visitor_can_see_all_trips_on_index_spec.rb
+++ spec/features/visitor_can_see_all_trips_on_index_spec.rb
@@ -17,5 +17,23 @@ describe 'Visits trips index page' do
       expect(page).to have_content(trip1.name)
       expect(page).to have_content(trip2.name)
     end
+
+    it "shows names of all trips" do
+      name = 'Punch Bowl'
+      name2 = 'Rincon'
+      start = '2018-05-28 08:37:48'
+      end_time = '2018-05-28 12:37:48'
+      start2 = '2018-05-28 02:37:48'
+      end_time2 = '2018-05-28 10:37:48'
+      trip1 = Trip.create!(name: name, start_date: start, end_date: end_time)
+      trip2 = Trip.create!(name: name2, start_date: start2, end_date: end_time2)
+
+      visit trips_path
+
+      click_link name
+
+      expect(page).to have_content(trip1.name)
+      expect(current_path).to eq(trip_path(trip1))
+    end
   end
 end
